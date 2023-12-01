680373cecb36528f239c83da671386ec8145090d
SteinRZ <xddan15@gmail.com>
2023-11-30 20:02:24 -0600

    Reestructuracion de main

--- html/main.php
+++ html/main.php
@@ -3,87 +3,84 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rosebrith | Inicio</title>
+    <title>Inicio - Rosebrith</title>
     <link rel="icon" href="../image/main_icon.png">
     <link rel="stylesheet" href="../css/navbar.css">
     <link rel="stylesheet" href="../css/style_main.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap">
 </head>
 
 <body>
     <!--------------------NAVBAR--------------------->
     <header class="navbar">
         <div class="navbar__icon">
-            <a href="main.php"><img src="../image/main_icon.png" alt="Rosebrith" title="Inicio"></a>
+            <a href="#contenido"><img src="../image/main_icon.png" alt="Rosebrith" title="Inicio"></a>
         </div>
         <nav>
            <ul class="nav__links">
-                <li><a href="services.php">Servicios</a></li>
-                <li><a href="contact_us.php">Contactanos</a></li>
+                <li><a href="#servicios">Servicios</a></li>
+                <li><a href="#contactanos">Contactanos</a></li>
            </ul>            
         </nav>
         <?php include '..\php\change_button.php';?>
     </header>
     
     <!--------------------CONTENT--------------------->
-    <div class="content" id="content">
-        <div class="content__image">
+    <div class="contenido" id="contenido">
+        <div class="contenido_imagen">
             <img src="../image/main_icon.png" alt="Rosebrith" class="img">
         </div>
-        <div class="content__title">
-            <h3>Reserva tu cita con nosotros</h3>
+        <div class="contenido__titulo">
+            <h3>Bienvenido a Rosebrith</h3>
         </div>
-        <div class="content__text">
-            <p>Disfruta de un dia refrescante reservando tu cita en Rosebrith.</p>
+        <div class="contenido__texto">
+            <p>Disfruta de un dia especial junto con nosotros en nuestro grandioso salon o nuestra grandiosa alberca.</p>
         </div>
-        <div class="content__button">
-            <a href="reserve.php"><button class="content__button--button" id="btn_reserva">¡Reserva ahora!</button></a>
+        <div class="contenido__texto--2">
+            <p>Ponte en contactos con nosotros y crea una reservacion ahora mismo con nosotros.</p>
         </div>
     </div>
     
     <!--------------------SERVICES-------------------->
-    <!----------BACKGROUND--------->
-    <div class="ocean">
-        <!-------IMAGES CONTENT------------->
-        <div class="services">
-            <div class="services_title"> 
-                <h2>Nuestros Servicios</h2>
+    <div class="servicios" id="servicios">
+        <div class="servicios__titulo">
+            <h3>Nuestros servicios</h3>
+        </div>
+        <div class="servicios__texto">
+            <p>En Rosebrith, distruta de un dia especial en nuestro salon de eventos para hasta 60 personas o de nuestra grandiosa alberca.</p>
+        </div>
+        <div class="servicios__imagenes">
+            <div class="servicios__imagenes--1">
+                <img src="../image/img_pool_1.jpg" alt="Imagen alberca" class="img2">
             </div>
-            <div class="services_image">         
-            <!---------------POOL------------->  
-                <article class="card">
-                    <img class="card__background" src="../image/img_pool_1.jpg" alt="Alberca">
-                    <div class="card__content | flow">
-                        <div class="card__content--container | flow">
-                            <h2 class="card__title">Alberca</h2>
-                            <p class="card__description">Pasa con nosotros un dia refrescante en nuestra gran Alberca.</p>
-                        </div>
-                        <a href="services.php"><button class="card__button">Conoce más</button></a href>
-                    </div>
-                </article>
-                <!-------------LOUNGE----------->
-                <article class="card">
-                    <img class="card__background" src="../image/img_eventhall_2.jpg" alt="Salon">
-                    <div class="card__content | flow">
-                        <div class="card__content--container | flow">
-                            <h2 class="card__title">Salón</h2>
-                            <p class="card__description">Pasa con nosotros tu evento más importante en nuestro gran Salon.</p>
-                        </div>
-                        <a href="services.php"><button class="card__button">Conoce más</button></a href>
-                    </div>
-                </article>
-            <!--------------END IMAGE CARDS---------------------->   
+            <div class="servicios__imagenes--2">
+                <img src="../image/img_pool_2.jpg" alt="Imagen alberca" class="img2">
+            </div>
+            <div class="servicios__imagenes--3">
+                <img src="../image/img_eventhall_1.jpg" alt="Imagen alberca" class="img2">
+            </div>
+            <div class="servicios__imagenes--4">
+                <img src="../image/img_eventhall_2.jpg" alt="Imagen alberca" class="img2">
             </div>
         </div>
-        <div class="bubble bubble--1"></div>
-        <div class="bubble bubble--2"></div>
-        <div class="bubble bubble--6"></div>
-        <div class="bubble bubble--1"></div>
-        <div class="bubble bubble--2"></div>
-        <div class="bubble bubble--3"></div>
-        <div class="bubble bubble--7"></div>
-        <!---------------END IMAGES CONTENT-->
     </div>
-    <!---END BACKGROUND-->
+
+    <!--------------------CONTACT-------------------->
+    <div class="contactanos" id="contactanos">
+        <div class="contactanos__titulo">
+            <h3>Contactanos</h3>
+        </div>
+        <p>
+            Telefono:
+        </p>
+        <p>
+            Whatsapp:
+        </p>
+        <p>
+            Correo:
+        </p>
+        <p>
+            Donde encontrarnos: Colonia Union del Recuerdo, Calle Recuerdo Ave La Cruz #812
+        </p>
+    </div>
 </body>
 </html>
